a23c53bd4787efeccc29374d15c2e40c2237e6ff
Juliano <julianobuzatto02@gmail.com>
2025-10-18 17:21:19 -0300

    adicionado comandos para carro e teste

--- ProjectEstacionamento/Program.cs
+++ ProjectEstacionamento/Program.cs
@@ -20,6 +20,43 @@ var app = builder.Build();
 
 app.MapGet("/", () => "API Estacionamento");
 
+// ----------------- CARRO -----------------
 
+app.MapPost("/api/carro/cadastrar", ([FromBody] Carro carro, [FromServices] AppDataContext ctx) =>
+{
+    if (ctx.Carros.Any(c => c.Placa == carro.Placa))
+        return Results.Conflict("Carro já cadastrado!");
+
+    ctx.Carros.Add(carro);
+    ctx.SaveChanges();
+    return Results.Created("/api/carro/cadastrar", carro);
+});
+// ------------------ LISTAR CARROS -----------------
+app.MapGet("/api/carro/listar", ([FromServices] AppDataContext ctx) =>
+{
+    var lista = ctx.Carros.Include(c => c.Vaga).ToList();
+    return lista.Any() ? Results.Ok(lista) : Results.BadRequest("Nenhum carro cadastrado!");
+});
+// ----------------- REMOVER CARRO -----------------
+app.MapDelete("/api/carro/remover/{id}", ([FromRoute] string id, [FromServices] AppDataContext ctx) =>
+{
+    var carro = ctx.Carros.Find(id);
+    if (carro is null) return Results.NotFound("Carro não encontrado!");
+
+    ctx.Carros.Remove(carro);
+    ctx.SaveChanges();
+    return Results.Ok(carro);
+});
+// ----------------- ALTERAR CARRO -----------------
+app.MapPatch("/api/carro/alterar/{id}", ([FromRoute] string id, [FromBody] Carro carroAlterado, [FromServices] AppDataContext ctx) =>
+{
+    var carro = ctx.Carros.Find(id);
+    if (carro is null) return Results.NotFound("Carro não encontrado!");
+
+    carro.Placa = carroAlterado.Placa;
+    carro.Modelo = carroAlterado.Modelo;
+    ctx.SaveChanges();
+    return Results.Ok(carro);
+});
 
 app.Run();
